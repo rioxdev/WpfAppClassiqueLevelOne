0885ac4e8b4089af1fb6c86b441f9fcfc4260803
henrio randriasoa <rioxdev@gmail.com>
2021-09-16 21:54:05 +0200

    initial commit

---
+++ WpfAppClassiqueLevelOne/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppClassiqueLevelOne
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            RenderButton();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($" it s my time {DateTime.Now}");
+        }
+
+        private void RenderButton()
+        {
+            Button btn = new Button()
+            {
+                Content = DateTime.Now.ToShortTimeString(),
+                Width = 50,
+                Height = 40
+            };
+
+            btn.Click += Btn_Click;
+
+            this.Content = btn;
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($" it s my time {DateTime.Now}");
+        }
+    }
+}
